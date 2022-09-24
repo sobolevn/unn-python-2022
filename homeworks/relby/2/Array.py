d1105b2c4a7388951fc756298603ed52d2f1aa90
relby <kudinov.nikita@gmail.com>
2022-09-25 00:20:25 +0300

    Inherit from MutableSequence

--- homeworks/relby/2/Array.py
+++ homeworks/relby/2/Array.py
@@ -1,4 +1,4 @@
-from typing import Generic, Iterator, SupportsIndex, TypeVar, Iterable, overload
+from typing import MutableSequence, Generic, TypeVar, Iterator, SupportsIndex, Iterable, overload
 
 T = TypeVar('T')
 
@@ -6,7 +6,7 @@ T = TypeVar('T')
 # when python 3.11 release comes out
 
 
-class Array(Generic[T]):
+class Array(MutableSequence[T], Generic[T]):
     def __init__(self, *args: T) -> None:
         self._data: tuple[T, ...] = args
 
@@ -20,39 +20,8 @@ class Array(Generic[T]):
     def append(self, value: T) -> None:
         self._data += (value,)
 
-    def extend(self, iterable: Iterable[T]) -> None:
-        self._data += tuple(iterable)
-
-    def pop(self, index: SupportsIndex | None = None) -> T:
-        if not isinstance(index, SupportsIndex) and index is not None:
-            raise TypeError(
-                "'{0}' object cannot be interpreted as an integer".format(
-                    index.__class__.__name__,
-                ),
-            )
-        if self.empty():
-            raise IndexError('pop from empty Array')
-
-        ind = len(self) - 1 if index is None else index.__index__()
-        if index is None:
-            ind = len(self) - 1
-        else:
-            ind = x if (x := index.__index__()) > 0 else len(self) + x
-            if ind < 0 or ind >= len(self):
-                raise IndexError('pop index out of range')
-
-        item = self._data[ind]
-        self._data = self._data[:ind] + self._data[ind + 1:]
-        return item
-
-    def clear(self) -> None:
-        self._data = ()
-
     def insert(self, index: SupportsIndex, value: T) -> None:
-        self._data = self._data[index:] + (value,) + self._data[index:]
-
-    def reverse(self) -> None:
-        self._data = self._data[::-1]
+        self._data = self._data[int(index):] + (value,) + self._data[int(index):]
 
     def remove(self, value: T) -> None:
         index = self.index(value)
@@ -64,63 +33,24 @@ class Array(Generic[T]):
     def empty(self) -> bool:
         return not len(self)
 
-    def copy(self) -> 'Array[T]':
-        return self[:]
-
-    def index(self, value: T) -> int:
+    def index(
+        self,
+        value: T,
+        start: SupportsIndex | None = None,
+        stop:  SupportsIndex | None = None,
+    ) -> int:
+        if not isinstance(start, SupportsIndex) or not isinstance(stop, SupportsIndex):
+            raise TypeError('slice indices must be integers or have an __index__ method')
+        start = start or 0
+        stop  = stop  or len(self)
         try:
             return self._data.index(value)
         except ValueError:
             return -1
 
-    def count(self, value: T) -> int:
-        return self._data.count(value)
-
     # Dunder methods
-    @overload
-    def __getitem__(self, key: SupportsIndex, /) -> T: ...
-
-    @overload
-    def __getitem__(self, key: slice, /) -> 'Array[T]': ...
-
-    def __getitem__(self, key: SupportsIndex | slice, /) -> T | 'Array[T]':
-        match key:
-            case slice():
-                return Array(*self._data[key])
-            case SupportsIndex():
-                return self._data[key]
-            case _:
-                raise TypeError(
-                    "Array indices must be integers or slices, not '{0}'".format(
-                        key.__class__.__name__,
-                    ),
-                )
-
-    @overload
-    def __setitem__(self, key: SupportsIndex, value: T, /) -> None: ...
-
-    @overload
-    def __setitem__(self, key: slice, value: Iterable[T], /) -> None: ...
-
-    def __setitem__(self, key: SupportsIndex | slice, value: T | Iterable[T], /) -> None:
-        match key:
-            case slice():
-                if not isinstance(value, Iterable):
-                    raise TypeError('can only assign to iterable')
-                # Don't wanna implement this myself
-                data = list(self._data)
-                data[key] = value
-                self._data = tuple(data)
-            case SupportsIndex():
-                self._data = self._data[:key] + (value,) + self._data[key + 1:]  # type: ignore
-            case _:
-                raise TypeError(
-                    "Array indices must be integers or slices, not '{0}'".format(
-                        key.__class__.__name__,
-                    ),
-                )
-
-    def __add__(self, array: 'Array[T]', /) -> 'Array[T]':
+    # Math
+    def __add__(self, array: 'Array[T]') -> 'Array[T]':
         if isinstance(array, Array):
             return Array(*self, *array)
         raise TypeError(
@@ -129,7 +59,7 @@ class Array(Generic[T]):
             ),
         )
 
-    def __mul__(self, n: SupportsIndex, /) -> 'Array[T]':
+    def __mul__(self, n: SupportsIndex) -> 'Array[T]':
         if isinstance(n, SupportsIndex):
             return Array(*self._data * n)
         raise TypeError(
@@ -141,18 +71,21 @@ class Array(Generic[T]):
     def __len__(self) -> int:
         return len(self._data)
 
+    def __contains__(self, value: object) -> bool:
+        return value in self._data
+
     # Comparisons
-    def __eq__(self, other: object, /) -> bool:
+    def __eq__(self, other: object) -> bool:
         if isinstance(other, Array):
             return self._data == other._data
         return False
 
-    def __lt__(self, other: object, /) -> bool:
+    def __lt__(self, other: object) -> bool:
         if isinstance(other, Array):
             return self._data < other._data
         return False
 
-    def __le__(self, other: object, /) -> bool:
+    def __le__(self, other: object) -> bool:
         if isinstance(other, Array):
             return self._data <= other._data
         return False
@@ -172,3 +105,76 @@ class Array(Generic[T]):
             types,
             self,
         )
+
+    @overload
+    def __getitem__(self, key: SupportsIndex) -> T: ...
+
+    @overload
+    def __getitem__(self, key: slice) -> 'Array[T]': ...
+
+    def __getitem__(self, key: SupportsIndex | slice) -> T | 'Array[T]':
+        match key:
+            case slice():
+                return Array(*self._data[key])
+            case SupportsIndex():
+                if not self._is_valid_index(key):
+                    raise IndexError('Array index out of range')
+                return self._data[key]
+            case _:
+                raise TypeError(
+                    "Array indices must be integers or slices, not '{0}'".format(
+                        key.__class__.__name__,
+                    ),
+                )
+
+    @overload
+    def __setitem__(self, key: SupportsIndex, value: T) -> None: ...
+
+    @overload
+    def __setitem__(self, key: slice, value: Iterable[T]) -> None: ...
+
+    def __setitem__(self, key: SupportsIndex | slice, value: T | Iterable[T]) -> None:
+        match key:
+            case slice():
+                if not isinstance(value, Iterable):
+                    raise TypeError('can only assign to iterable')
+                # Don't wanna implement this myself
+                data = list(self._data)
+                data[key] = value
+                self._data = tuple(data)
+            case SupportsIndex():
+                if not self._is_valid_index(key):
+                    raise IndexError('Array assignment index out of range')
+                self._data = self._data[:key] + (value,) + self._data[int(key) + 1:]  # type: ignore
+            case _:
+                raise TypeError(
+                    "Array indices must be integers or slices, not '{0}'".format(
+                        key.__class__.__name__,
+                    ),
+                )
+
+    @overload
+    def __delitem__(self, key: SupportsIndex) -> None: ...
+
+    @overload
+    def __delitem__(self, key: slice) -> None: ...
+
+    def __delitem__(self, key: SupportsIndex | slice) -> None:
+        match key:
+            case slice():
+                self[key] = ()
+            case SupportsIndex():
+                if not self._is_valid_index(key):
+                    raise IndexError('Array deletion index out of range')
+                self._data = self._data[:key] + self._data[int(key) + 1:]  # type: ignore
+            case _:
+                raise TypeError(
+                    "Array indices must be integers or slices, not '{0}'".format(
+                        key.__class__.__name__,
+                    ),
+                )
+
+    # Private methods
+    def _is_valid_index(self, index: SupportsIndex) -> bool:
+        ind = int(index)
+        return -len(self) <= ind and ind < len(self)
