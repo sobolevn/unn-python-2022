619cfc859505fa0e90a9a64e02c97fba22dd7413
ALTokarev7 <sasha_tokarev_02@mail.ru>
2022-09-24 15:57:20 +0300

    Array realization

--- homeworks/ALTokarev7/1/array.py
+++ homeworks/ALTokarev7/1/array.py
@@ -29,9 +29,21 @@ class Array(object):
         return -1
 
 def main():
-    array_a = Array()
-    array_b = array_a + Array(1,2,3)
-    print(Array(1,2,3,4,5))
+    mas_a = Array(1,2) 
+    print(f'first array = {mas_a}' )
+    mas_b = Array(3,4,5,6)
+    print(f'second array = {mas_b}' )
+    mas_a.append(33) 
+    print(f'first array after append(33) = {mas_a}')
+    mas_sum = mas_a + mas_b  
+    print(f'array of sum first and second: {mas_sum}')
+    print(f'length of sum array is {len(mas_sum)}')
+    print(f'index of element 1 in sum array is {mas_sum.index(1)}')
+
+    for el in mas_sum: #array working with for loop
+        print(el)
+
+    print(f'Sum array el number [1] is {mas_sum[1]}')
 
 if __name__ == "__main__":
     main()
