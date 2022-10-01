953245c7b7e6b0489a6b01cea72045d67e65b9ae
relby <kudinov.nikita@gmail.com>
2022-10-02 00:57:56 +0300

    Lint the code

--- practice/relby/1/game.py
+++ practice/relby/1/game.py
@@ -1,15 +1,18 @@
 # `random` module is used to shuffle field, see:
 # https://docs.python.org/3/library/random.html#random.shuffle
 import random
-import sys, tty, termios
+import sys
+import termios
+import tty
+
+from typing import Final
 
 # Empty tile, there's only one empty cell on a field:
-EMPTY_MARK = 'x'
+EMPTY_MARK: Final = 'x'
 
 # Dictionary of possible moves if a form of:
-# key -> delta to move the empty tile on a field.
-# delta: (dy, dx)
-MOVES = {
+# key -> (dy, dx) to move the empty tile on a field.
+MOVES: Final = {
     'w': (+1, 0),
     's': (-1, 0),
     'a': (0, +1),
@@ -19,29 +22,37 @@ MOVES = {
 # Make an alias to field
 Field = list[list[int]]
 
+
 # Helper functions
 def field_from_flat_field(flat_field: list[int], size: int) -> Field:
     """
-    This function returns field from flat_field
-    (flat_field is one dimensional list, and field is two dimensional)
+    This function returns field from flat_field.
 
-    :param flat_field: Flat field
-    :param size: Size of each row
-    :return: Field object
+    (flat_field is one dimensional list, and field is two dimensional)
     """
-    return [flat_field[i*size:i*size + size] for i in range(size)]
+    field = []
+    for ind in range(size):
+        field_slice = slice(ind*size, ind*size + size)
+        field.append(flat_field[field_slice])
+    return field
+
 
 def deep_copy_field(field: Field) -> Field:
     return [row[:] for row in field]
 
-# Consts
-FIELD_SIZE = 4
-EMPTY_CELL = FIELD_SIZE**2 - 1
-WINNING_FIELD = field_from_flat_field(list(range(FIELD_SIZE**2)), FIELD_SIZE)
 
-PADDING = len(str(EMPTY_CELL)) + 1
+# Consts
+FIELD_SIZE: Final = 4
+EMPTY_CELL: Final = FIELD_SIZE**2 - 1
+WINNING_FIELD: Final = field_from_flat_field(
+    list(range(FIELD_SIZE**2)),
+    FIELD_SIZE,
+)
+
+PADDING: Final = len(str(EMPTY_CELL)) + 1
 # This should grow depending on FIELD_SIZE
-SHUFFLE_ITERATIONS = FIELD_SIZE*1000
+SHUFFLE_ITERATIONS: Final = FIELD_SIZE*1000
+
 
 # Field functions
 def shuffle_field() -> Field:
@@ -61,6 +72,7 @@ def shuffle_field() -> Field:
         field = new_field
     return field
 
+
 def print_field(field: Field) -> None:
     """
     This function prints field to user.
@@ -70,14 +82,19 @@ def print_field(field: Field) -> None:
     """
     for row in field:
         for cell in row:
-            cell_to_render = EMPTY_MARK if cell == EMPTY_CELL else str(cell + 1)
-            print(cell_to_render.rjust(PADDING), end='')
-        print()
+            if cell == EMPTY_CELL:
+                cell_to_render = EMPTY_MARK
+            else:
+                cell_to_render = str(cell + 1)
+            sys.stdout.write(cell_to_render.rjust(PADDING))
+        sys.stdout.write('\n')
+
 
 def reprint_field(field: Field) -> None:
-    print('\x1b[{0}A'.format(FIELD_SIZE), end='')
-    print('\x1b[{0}D'.format(FIELD_SIZE*PADDING), end='')
+    sys.stdout.write('\x1b[{0}A'.format(FIELD_SIZE))
+    sys.stdout.write('\x1b[{0}D'.format(FIELD_SIZE*PADDING))
     print_field(field)
+
 
 def is_game_finished(field: Field) -> bool:
     """
@@ -98,7 +115,6 @@ def perform_move(field: Field, key: str) -> Field | None:
     :return: new field state (after the move)
         or `None` if the move can't me done.
     """
-
     move = MOVES.get(key)
     if move is None:
         return None
@@ -115,11 +131,15 @@ def perform_move(field: Field, key: str) -> Field | None:
     dy, dx = move
     y2, x2 = (y1 + dy, x1 + dx)
 
-    if y2 < 0 or y2 >= FIELD_SIZE or x2 < 0 or x2 >= FIELD_SIZE:
+    if y2 < 0 or y2 >= FIELD_SIZE:
         return None
+    if x2 < 0 or x2 >= FIELD_SIZE:
+        return None
+
     new_field = deep_copy_field(field)
     new_field[y1][x1], new_field[y2][x2] = new_field[y2][x2], new_field[y1][x1]
     return new_field
+
 
 # Terminal magic (works only on Unix-like systems)
 # I use this to render each mutation to the field
@@ -133,8 +153,10 @@ def init_term():
     termios.tcsetattr(fd, termios.TCSANOW, newt)
     return fd, oldt
 
+
 def revert_term(fd, oldt):
     termios.tcsetattr(fd, termios.TCSANOW, oldt)
+
 
 def main() -> None:
     """
@@ -152,13 +174,15 @@ def main() -> None:
             print('You have won!')
             break
         key = sys.stdin.read(1)
-        if key == 'q': break
+        if key == 'q':
+            break
         new_field = perform_move(field, key)
         if new_field is not None:
             field = new_field
             reprint_field(field)
 
     revert_term(fd, oldt)
+
 
 if __name__ == '__main__':
     # See what this means:
