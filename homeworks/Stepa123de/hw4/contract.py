bdf6ce8a4b2ccc993229bc341fd7c63adca8dc8a
Luigi_Capello <Stepa.kashin.2018@gmail.com>
2022-10-02 17:45:42 +0300

    HW3

---
+++ homeworks/Stepa123de/hw4/contract.py
@@ -0,0 +1,40 @@
+
+
+class ContractError(Exception):
+    """Класс исключений при несоответсвие значений типа аргумента и необходимого"""
+    def __init__(self,*args):
+        self.messege = args[0] if args else None
+
+    def __str__(self):
+        return f"Mistake: {self.messege}"
+
+#: Special value, that indicates that validation for this type is not required.
+Any = object()
+
+def contract(arg_types=None, return_type=None, raises=None):
+    def decorator(func):
+        def wrapped(*args, **kwargs):
+            for index,value in enumerate(tuple((*args,*kwargs))):
+                if type(value) is not Any and type(value) != arg_types[index]:
+                    raise ContractError("Type argument not support") from TypeError
+
+            try:
+                res = func(*args, **kwargs);
+            except (Exception if raises is None or Any in raises else raises) as ex:
+                raise ex          
+            except Exception as ex: 
+                raise ContractError("Error isn't in raises: " + type(ex).__name__) from ex
+
+            if type(value) is not Any and type(res) != return_type:
+                raise ContractError("Type return not support") from TypeError
+
+            return res
+        return wrapped
+    return decorator
+
+@contract(arg_types=(int, int), return_type=float, raises=(NameError,))
+def sum(first, second):
+    return first / second
+
+
+print(sum(0,0))
