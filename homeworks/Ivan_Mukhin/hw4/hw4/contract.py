e616cdde66b6cc8ac7e4483fdc7da70c26555d21
VankaVstanka03 <vvstanka39@gmail.com>
2022-10-04 19:56:18 +0300

    added hw4

--- homeworks/Ivan_Mukhin/hw4/hw4/contract.py
+++ homeworks/Ivan_Mukhin/hw4/hw4/contract.py
@@ -1,4 +1,4 @@
-class ContractError(Excepetion):
+class ContractError(Exception):
     """We use this error when someone breaks our contract."""
 
 
@@ -7,4 +7,41 @@ Any = object()
 
 
 def contract(arg_types=None, return_type=None, raises=None):
-    ...  # TODO: implement
+    def error_processing(function):
+        def decor(*args, **kwargs):
+            if arg_types != None:
+                for i, elem in enumerate(arg_types):
+                    if elem != Any and arg_types[i] != type(args[i]):
+                        raise ContractError('Wrong arguments')
+            try:
+                res = function(*args, **kwargs)
+            except (raises if raises is not None and Any not in raises else Exception) as ex:
+                raise ex
+            except Exception:
+                raise ContractError from Exception
+            if return_type is not None and not isinstance(res, return_type):
+                raise ContractError('Return types not equal')
+
+            return res
+        return decor
+    return error_processing
+
+
+
+
+
+
+
+
+
+@contract(arg_types=(float, int), return_type=int, raises=None)
+def add_two_numbers(first, second):
+    return first + second
+
+add_two_numbers('a', 'b')
+
+@contract(arg_types=(int, int), return_type=float, raises=(ZeroDivisionError,))
+def div(first, second):
+    return first / second
+
+div(1, 2)
