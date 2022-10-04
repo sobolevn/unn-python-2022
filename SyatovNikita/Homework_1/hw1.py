facc9b58ece6794a4d21c0eb54d8c5d18afcecf8
NikitaSyatov <syatov430@mail.ru>
2022-10-04 19:17:06 +0300

    Modified hw1 and hw2

--- SyatovNikita/Homework_1/hw1.py
+++ SyatovNikita/Homework_1/hw1.py
@@ -1,36 +1,16 @@
+questions = ("Какая версия языка сейчас актуальна?",
+             "Какая кодировка используется в строках?",
+             "Какой оператор сравнения нужно использовать для работы с None и bool?",
+             "Сколько значений есть у bool?")
 answers = ("Python3", "UTF8", "is", "2")
-countTrueAnswer = 0
+count_correct_answer = 0
 
-print("Игра в загадки\nКакая версия языка сейчас актуальна?")
-user_answer = input()
-if user_answer.casefold() == answers[0].casefold():
-    print("Ответ " + user_answer + " верен")
-    countTrueAnswer += 1
-else:
-    print("Неверный ответ")
+for number_question in range(0, len(questions)):
+    user_answer = input(questions[number_question])
+    if user_answer.casefold() == answers[number_question].casefold():
+        print("Ответ " + user_answer + " верен")
+        count_correct_answer += 1
+    else:
+        print("Неверный ответ")
 
-print("Какая кодировка используется в строках?")
-user_answer = input()
-if user_answer.casefold() == answers[1].casefold():
-    print("Ответ " + user_answer + " верен")
-    countTrueAnswer += 1
-else:
-    print("Неверный ответ")
-
-print("Какой оператор сравнения нужно использовать для работы с None и bool?")
-user_answer = input()
-if user_answer.casefold() == answers[2].casefold():
-    print("Ответ " + user_answer + " верен")
-    countTrueAnswer += 1
-else:
-    print("Неверный ответ")
-
-print("Сколько значений есть у bool?")
-user_answer = input()
-if user_answer.casefold() == answers[3].casefold():
-    print("Ответ " + user_answer + " верен")
-    countTrueAnswer += 1
-else:
-    print("Неверный ответ")
-
-print("Дано " + str(countTrueAnswer) + " правильных ответов")
+print("Дано " + str(count_correct_answer) + " правильных ответов")
