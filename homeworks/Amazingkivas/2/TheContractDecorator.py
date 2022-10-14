22d7dde5a39dad73d4cf9218b9653545975aed43
SaveliyBorisov <borisov.talim@yandex.ru>
2022-10-14 16:12:11 +0300

    add: docstring and zip, fix: exceptions check, arg_types check and value Any

--- homeworks/Amazingkivas/2/TheContractDecorator.py
+++ homeworks/Amazingkivas/2/TheContractDecorator.py
@@ -3,43 +3,33 @@ class ContractError(Exception):
 
 
 #: Special value, that indicates that validation for this type is not required.
-Any = object()
+Any = type(object())
 
 
 def contract(arg_types=None, return_type=None, raises=None):
+    """This function create contract decorator with the specified conditions"""
     def contract_decorator(function):
         def wrapped(*args, **kwargs):
             if arg_types is not None:
-                for index in range(len(arg_types)):
-                    this_type = arg_types[index]
-                    this_arg = (args + tuple(kwargs.values()))[index]
-                    if this_type is not Any:
-                        if not isinstance(this_arg, this_type):
-                            raise ContractError('Invalid argument type')
+                all_args = (args + tuple(kwargs.values()))
+                for arg_value, arg_type in zip(all_args, arg_types):
+                    if not isinstance(arg_value, arg_type):
+                        raise ContractError('Invalid argument type')
 
-            try:
+            if raises is None or Any in raises:
                 return_value = function(*args, **kwargs)
-            except (Exception if raises is None or
-                    Any in raises else raises) as ex:
-                raise ex
-            except Exception as error:
-                raise ContractError from error
+            else:
+                try:
+                    return_value = function(*args, **kwargs)
+                except raises as ex:
+                    raise ex
+                except Exception as ex:
+                    raise ContractError from ex
 
             if return_type is not None:
                 if not isinstance(return_value, return_type):
                     raise ContractError('Invalid return type')
 
             return return_value
-
         return wrapped
-
     return contract_decorator
-
-
-#: Check
-@contract(arg_types=(int, Any), return_type=str, raises=(IndexError,))
-def return_exclaim_string(string, array):
-    return array[string] + '!'
-
-
-print(return_exclaim_string(string=0, array=('Hey', 'Wait')))
