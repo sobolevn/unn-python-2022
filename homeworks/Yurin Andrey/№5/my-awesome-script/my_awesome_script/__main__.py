8b1bc976f73eb0877ea45bf96c20141a95bc3d24
Namxobick <yurinsin@gmail.com>
2022-10-19 19:58:24 +0300

    Corrected code (flake8)

--- homeworks/Yurin Andrey/№5/my-awesome-script/my_awesome_script/__main__.py
+++ homeworks/Yurin Andrey/№5/my-awesome-script/my_awesome_script/__main__.py
@@ -1,17 +1,18 @@
-from .cmdInput import cmd_input
+
+from .cmd_input import cmd_input
 
 
 def main():
+    """Main method."""
     try:
         cmd_input()
     except Exception as ex:
-        print('You have done something wrong!', ex)
+        print('You have done something wrong!', ex)  # noqa: WPS421
 
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print()
-        print('Shutting down, bye!')
-
+        print()  # noqa: WPS421
+        print('Shutting down, bye!')  # noqa: WPS421
--- homeworks/Yurin Andrey/№5/my-awesome-script/my_awesome_script/command.py
+++ homeworks/Yurin Andrey/№5/my-awesome-script/my_awesome_script/command.py
@@ -1,33 +1,43 @@
+
+from datetime import datetime
+
+import pytz
+from cowpy import cow
 from pygments import highlight
 from pygments.formatters import TerminalFormatter
 from pygments.lexers import PythonLexer
-from cowpy import cow
-from datetime import datetime
-import pytz
 
 
 def command(operation: str, text: str = None) -> None:
-    functions[operation](text) if operation != "help" else functions[operation]()
+    """Function selection."""
+    if operation == 'help':
+        functions[operation]()
+    else:
+        functions[operation](text)
 
 
-def highlight_(code: str) -> None:
-    print(highlight(code, PythonLexer(), TerminalFormatter()))
+def highlights(code: str) -> None:
+    """Highlight."""
+    print(highlight(code, PythonLexer(), TerminalFormatter()))  # noqa: WPS421
 
 
-def cowsay_(text: str) -> None:
-    print(cow.milk_random_cow(text))
+def cowsay(text: str) -> None:
+    """Cowsay."""
+    print(cow.milk_random_cow(text))  # noqa: WPS421
 
 
-def time_(timezone: str) -> None:
+def time(timezone: str) -> None:
+    """Time."""
     local_date = datetime.now(pytz.timezone(timezone))
-    print("{0} {1}".format(local_date.date(), local_date.time()))
+    print('{0} {1}'.format(local_date.date(), local_date.time()))  # noqa: WPS421
 
 
-def help_() -> None:
-    print("EXAMPLE COMMAND:")
-    print(" highlight 'any text'")
-    print(" cowsay 'any text'")
-    print(" time 'Region/City' (example: Europe/Moscow)")
+def custom_help() -> None:
+    """Help."""
+    print('EXAMPLE COMMAND:')  # noqa: WPS421
+    print(" highlight 'any text'")  # noqa: WPS421
+    print(" cowsay 'any text'")  # noqa: WPS421
+    print(" time 'Region/City' (example: Europe/Moscow)")  # noqa: WPS421
 
 
-functions = {'highlight': highlight_, "cowsay": cowsay_, "time": time_, "help": help_}
+functions = {'highlight': highlights, 'cowsay': cowsay, 'time': time, 'help': custom_help}
