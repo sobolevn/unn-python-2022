9c97217556a4c78400296113bf7073b3c9ae840f
Namxobick <100288192+Namxobick@users.noreply.github.com>
2022-10-19 19:53:42 +0300

    Update cmdInput.py

--- homeworks/Yurin Andrey/№5/my-awesome-script/my_awesome_script/cmdInput.py
+++ homeworks/Yurin Andrey/№5/my-awesome-script/my_awesome_script/cmdInput.py
@@ -6,7 +6,7 @@ from .command import command, functions
 def cmd_input():
     """Input method."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('operation', help='input command', choices=functions)
+    parser.add_argument('operation', help='Input command', choices=functions)
     parser.add_argument('parameter', help="'Any text' or 'Region/City'")
     args = parser.parse_args()
 
