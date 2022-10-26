f079843a895fb0aa5d0ee24b91ba6fbe5578ca5b
Khramov Ivan <90196856+At0mn1yIvan@users.noreply.github.com>
2022-10-26 23:53:02 +0300

    Update tskInput.py

--- homeworks/At0mn1yIvan/5/my_awesome_script/tskInput.py
+++ homeworks/At0mn1yIvan/5/my_awesome_script/tskInput.py
@@ -5,12 +5,12 @@ from cmnds import functions, task
 
 def com_input():
     parser = argparse.ArgumentParser()
-    parser.add_argument("action", help="Input action.",  choices=functions)
-    parser.add_argument("text", help="highlight 'any text'/cowsay 'any text'/time 'Region/City'")  # noqa: E501
+    parser.add_argument('action', help='Input action.',  choices=functions)
+    parser.add_argument('text', help="highlight 'any text'/cowsay 'any text'/time 'Region/City'")  # noqa: E501
     args = parser.parse_args()
 
     try:
         task(args.action, args.text)
     except Exception as ex:
-        task("help")
+        task('help')
         raise ex
