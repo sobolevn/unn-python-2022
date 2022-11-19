bdd236efb02ef74bbf5fda45a4903a8d8455c9bb
relby <kudinov.nikita@gmail.com>
2022-11-20 00:13:19 +0300

    Rewrite with asyncio and httpx libraries (NEW HW)

--- homeworks/relby/6/csv_request_xml/__main__.py
+++ homeworks/relby/6/csv_request_xml/__main__.py
@@ -1,7 +1,9 @@
+import asyncio
 from typing import Final
 
 from loguru import logger
 
+from csv_request_xml.api_types import Email, User
 from csv_request_xml.reader import read_emails_from_csv
 from csv_request_xml.requester import get_user_attributes, get_users
 from csv_request_xml.saver import save_to_xml
@@ -13,25 +15,36 @@ def _configure_logger():
     logger.add(lambda _: exit(1), level='ERROR')  # noqa: WPS421
 
 
-def main() -> None:
+async def handle_email(email: Email, users: list[User]):
+    logger.info('Start working with `{0}`'.format(email))
+    user = next(
+        (user for user in users if user['email'] == email),
+        None,
+    )
+    if user is not None:
+        attrs = await get_user_attributes(user)
+        save_to_xml(user, attrs)
+    else:
+        logger.warning(
+            'User with this email `{0}` was not found'.format(email),
+        )
+
+
+async def async_main() -> None:
     _configure_logger()
     emails = read_emails_from_csv(EMAILS_FILEPATH)
-    users = get_users()
+    users = await get_users()
 
-    for email in emails:
-        logger.info('Start working with `{0}`'.format(email))
-        user = next(
-            (user for user in users if user['email'] == email),
-            None,
-        )
-        if user is not None:
-            attrs = get_user_attributes(user)
-            save_to_xml(user, attrs)
-        else:
-            logger.warning(
-                'User with this email `{0}` was not found'.format(email),
-            )
+    await asyncio.gather(*[
+        handle_email(email, users)
+        for email in emails
+    ])
+
+
+def main() -> None:
+    asyncio.run(async_main())
 
 
 if __name__ == '__main__':
     main()
+# noqa: WPS473 (I suppose it's a bug)
